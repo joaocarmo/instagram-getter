bb24f240c3b98abc410fa57636d8fe76ed671797
Joao Carmo <17721880+joaocarmo@users.noreply.github.com>
2020-03-21 18:12:13 +0000

    Boilerplate ready

---
+++ lib/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import InstagramGetter from './instagram-getter'
+import { debugPrint, addLocationChangeCallback, awaitElement } from './utils'
+
+const main = async () => {
+  const appEl = await awaitElement(process.env.ROOT_ELEMENT)
+  const container = document.createElement('div')
+  appEl.appendChild(container)
+  ReactDOM.render(<InstagramGetter />, container)
+}
+
+addLocationChangeCallback(() => {
+  main().catch(debugPrint)
+})
